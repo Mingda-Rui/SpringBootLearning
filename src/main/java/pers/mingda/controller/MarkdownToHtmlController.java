c8bfdaed4342a5b3105fd324c4f699c787b6aa9b
Mingda Rui <mingdarui@gmail.com>
2019-12-18 01:01:14 +0000

    RequestMapping

--- src/main/java/pers/mingda/controller/MarkdownToHtmlController.java
+++ src/main/java/pers/mingda/controller/MarkdownToHtmlController.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,7 +20,7 @@ public class MarkdownToHtmlController {
 
     private final String githubMarkdownApi = "https://api.github.com/markdown";
 
-    @GetMapping("md-to-html-test")
+    @RequestMapping("md-to-html-test")
     public String markdownToHtmlTest() {
         String exampleMarkdown;
         try {
