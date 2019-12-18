32a034d64a142998b0e4124c4daedd9f0de1f0e8
Mingda Rui <mingdarui@gmail.com>
2019-12-18 00:55:39 +0000

    Markdown To HTML Example Page Using Github Markdown API

---
+++ src/main/java/pers/mingda/controller/MarkdownToHtmlController.java
@@ -0,0 +1,50 @@
+package pers.mingda.controller;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
+@RestController
+public class MarkdownToHtmlController {
+
+    private final String githubMarkdownApi = "https://api.github.com/markdown";
+
+    @GetMapping("md-to-html-test")
+    public String markdownToHtmlTest() {
+        String exampleMarkdown;
+        try {
+            exampleMarkdown = readInExampleNoteFile();
+        } catch (Exception ex) {
+            System.err.println("failed to get the example Markdown file!" + ex.getMessage());
+            ex.printStackTrace();
+            exampleMarkdown = null;
+        }
+        System.out.println("read in example markdown file: \n" + exampleMarkdown);
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("text", exampleMarkdown);
+        headers.add("mode", "markdown");
+        headers.setContentType(MediaType.TEXT_PLAIN);
+        HttpEntity<String> request = new HttpEntity<>(headers);
+        ResponseEntity<String> responseEntityStr = restTemplate.postForEntity(githubMarkdownApi, request, String.class);
+        return responseEntityStr.getBody();
+    }
+
+    private String readInExampleNoteFile() throws IOException {
+        Path path = Paths.get("src/main/resources/Note.md");
+        BufferedReader reader = Files.newBufferedReader(path);
+        return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+    }
+}
